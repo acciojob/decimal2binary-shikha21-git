d1c1161bd127a49f6953afd6d54047a4c047ed4e
shikha21-git <64396488+shikha21-git@users.noreply.github.com>
2023-07-25 11:00:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 function decimalToBinary(num) {
   //Write you code here
-  
+	let binary = "";
+	while(num>0){
+		let digit = num%2;
+		binary = digit+binary;
+		num = num/2;
+	}
+  return binary;
 }
 
 window.decimalToBinary = decimalToBinary;
